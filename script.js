cdd053d7cd0f999a63f7984661a08622708f1961
rahmatullayevismoil <ismoilrahmatullayev000@gmail.com>
2021-11-20 19:28:55 +0900

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var sumInput1 = +prompt('Son kiriting', 0);
+var sumInput2 = +prompt('Son kiriting', 0);
+var sumInput3 = +prompt('Son kiriting', 0);
+var sumInput4 = +prompt('Son kiriting', 0);
+var sumInput5 = +prompt('Son kiriting', 0);
+
+function sumInput(){
+    var result = 0;
+ return   result += (sumInput1+sumInput2+sumInput3+sumInput4+sumInput5); 
+}
+
+console.log(sumInput());
+
+
